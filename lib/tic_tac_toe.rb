2eff73af4c1cea4e14e9e16890acbcbb4e3f56ea
Cedric David <cedric.david@btopenworld.com>
2018-08-30 08:49:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,12 +30,12 @@ class TicTacToe
   end
   def turn_count
     count = 9
-    @board. do |item|
+    @board.each do |item|
       if item == " "
         count--
       end
     end
     count
   end
-  
+
 end
