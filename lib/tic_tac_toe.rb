f1ebdb66ca1b1d22443168d864d04797cc2d31d2
Cedric David <cedric.david@btopenworld.com>
2018-08-30 09:40:52 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,97 +1,74 @@
 class TicTacToe
-  attr_accessor :board
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+
   WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
   ]
-
-  def initialize()
-    @board = Array.new(9, " ")
-  end
-
-  def play
-    while !over?
-      turn
-    end
-    if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cats Game!"
-    end
-  end
-
   def display_board
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-
+  def input_to_index(input)
+    index = input.to_i - 1
+  end
+  def move(index, token = "X")
+    @board[index] = token
+  end
+  def position_taken?(index)
+    @board[index] != " "
+  end
+  def valid_move?(index)
+    index.between?(0,9) && !position_taken?(index)
+  end
+  def turn_count
+    @board.count do |item|
+      item != " "
+    end
+  end
+  def current_player
+    turn_count.even? ? "X" : "O"
+  end
   def turn
-    display_board
-    puts "Please enter 1-9:"
+    puts "Please enter a number between 1-9"
     input = gets.strip
-    if !valid_move?(input)
+    index = input_to_index(input)
+    valid_move?(index) ? move(index, current_player)  : turn
+    display_board
+  end
+  def won?
+    WIN_COMBINATIONS.any? do |arr|
+      if @board[arr[0]] == @board[arr[1]] && @board[arr[1]] == @board[arr[2]] && position_taken?(arr[0])
+        return arr
+      end
+    end
+  end
+  def full?
+    @board.all? do |item|
+      item != " "
+    end
+  end
+  def draw?
+     full? && !won?
+  end
+  def over?
+    draw? || won?
+  end
+  def winner
+    won? ? @board[won?[0]] : nil
+  end
+  def play
+    until over?
       turn
     end
-    move(input, current_player)
-    display_board
-  end
+    puts winner ?  "Congratulations #{winner}!" : "Cats Game!"
 
-  def valid_move?(input)
-    input.to_i.between?(1,9) && !position_taken?(input.to_i-1)
-  end
-
-  def current_player
-    turn_count % 2 == 0 ? "X" : "O"
-  end
-
-  def full?
-    @board.all?{|token| token == "X" || token == "O"}
-  end
-
-  def over?
-    won? || draw?
-  end
-
-  def turn_count
-    @board.count{|token| token == "X" || token == "O"}
-  end
-
-  def move(location, token)
-    @board[location.to_i-1] = token
-  end
-
-  def won?
-    WIN_COMBINATIONS.detect do |combo|
-      position(combo[0]) == position(combo[1]) &&
-      position(combo[1]) == position(combo[2]) &&
-      position_taken?(combo[0])
-    end
-  end
-
-  def draw?
-    !won? && @board.all?{|token| token == "X" || token == "O"}
-  end
-
-  def winner
-    if winning_combo = won?
-      @winner = position(winning_combo.first)
-    end
-  end
-
-  def position(location)
-    @board[location.to_i]
-  end
-
-  def position_taken?(location)
-    !(position(location).nil? || position(location) == " ")
   end
 end
