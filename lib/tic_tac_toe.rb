cd8ff8d2e6a6f861b8b1f7f64c0a7fef683d551e
Cedric David <cedric.david@btopenworld.com>
2018-08-30 08:42:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,7 +23,7 @@ class TicTacToe
     @board[index] = token
   end
   def position_taken?(index)
-    !@board[index] === " " 
+    @board[index] !== " "
   end
   def valid_move?(index)
     @board[index] >= 0 && @board[index] <= 9 && !position_taken?(index) 
